5ddd2484557fca3d45648df0dbf8207bd039cab2
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-06-29 15:04:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -467,17 +467,39 @@ msg.delete();
 
 
 
-client.on('message', msg =>{
-    let message=msg;
-    if(message.content.startsWith("*bc")){
-        var args = message.content.split(' ').slice(1).join(' ');
-    msg.guild.members.forEach(m=>{
-        m.send(args.replace(/[user]/g,m)).catch();
-    if(message.attachments.first()){
-m.sendFile(message.attachments.first().url).catch();
-    }
-    })    ;
-    }
+client.on('message', message => {
+  if (message.author.id === client.user.id) return;
+  if (message.guild) {
+ let embed = new Discord.RichEmbed()
+  let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc') {
+  if (!args[1]) {
+message.channel.send("**#bc <message>**");
+return;
+}
+      message.guild.members.forEach(m => {
+ if(!message.member.hasPermission('ADMINISTRATOR')) return;
+          var bc = new Discord.RichEmbed()
+          .setAuthor(message.author.username, message.author.avatarURL)
+          .addField(' الـسيرفر', `${message.guild.name}`,true)
+          .addField(' الـمرسل ', `${message.author.username}!${message.author.discriminator}`,true)
+          .addField(' الرسالة ', args)
+          .setThumbnail(message.guild.iconURL)
+          .setColor('RANDOM')
+          m.send(`${m}`,{embed: bc});
+      });
+      const AziRo = new Discord.RichEmbed()
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setTitle('✅| جاري ارسال رسالتك ')
+      .addBlankField(true)
+      .addField('♨| عدد الاعضاء المرسل لهم ', message.guild.memberCount , true)
+      .addField('📝| الرسالة ', args)
+      .setColor('RANDOM')
+      message.channel.sendEmbed(embed);
+  }
+  } else {
+      return;
+  }
 });
 
 	
