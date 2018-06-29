f13c60a6a2b43d659273b054b22b3200c44feb77
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-06-29 02:24:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -358,8 +358,6 @@ client.on('guildMemberAdd', member => {
 ● ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ● 
 👑『اوامر ادارية』👑
 
-👑 *ban 『لتعطي شخص باند』
-
 👑 *kick 『لتعطي شخص كيك』
 
 👑 *clear 『لمسح الشات برقم』
@@ -572,46 +570,7 @@ client.on('message', message => {
 	
 	
 	
-client.on('message', message => {
-  let prefix = "*"
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
-      console.log(`Diamond Team`);
- 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
 
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "ban") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-  if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("**You Don't Have ` BAN_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("BAN_MEMBERS")) return message.reply("**I Don't Have ` BAN_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  let b5bzlog = client.channels.find("name", "mod-log");
-
-  if(!b5bzlog) return message.reply("I've detected that this server doesn't have a 5bz-log text channel.");
-  if (message.mentions.users.size < 1) return message.reply("**منشن شخص**");
-  if(!reason) return message.reply ("**اكتب سبب الطرد**");
-  if (!message.guild.member(user)
-  .bannable) return message.reply("**لايمكنني طرد شخص اعلى من رتبتي يرجه اعطاء البوت رتبه عالي**");
-
-  message.guild.member(user).ban(7, user);
-
-  const banembed = new Discord.RichEmbed()
-  .setAuthor(`BANNED!`, user.displayAvatarURL)
-  .setColor("RANDOM")
-  .setTimestamp()
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-  message.channel.send({
-    embed : banembed
-  })
-}
-});
 	
 	
 	
