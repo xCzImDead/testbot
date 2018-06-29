01ae7c5c026da0833a0a07d750d734cf51e5683f
xCzImDead <40675576+xCzImDead@users.noreply.github.com>
2018-06-29 20:48:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -294,20 +294,20 @@ client.on('guildMemberAdd', member => {
 
    
    
-   client.on('message', message => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-  
-    let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);
-  
-    let args = message.content.split(" ").slice(1);
-  
-    if (command == "*say") {
-     message.channel.sendMessage(args.join("  "))
-     message.delete()
-    }
-   });
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "*say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
    
    
